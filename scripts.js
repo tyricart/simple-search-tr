788133c0e3a5052d1eb39ef91d4ea185bcf306b4
Tyrene Ricart <tyrenericart@Tyrenes-MacBook-Pro.local>
2022-07-19 22:41:59 -0400

    first push

---
+++ scripts.js
@@ -0,0 +1,23 @@
+function myFunction(){
+    let input, filter, table, tr, td, txtValue, txtValue2;
+    input = document.getElementById('myInput');
+    filter = input.value.toUpperCase();
+    table = document.getElementById('myTable');
+    tr = table.getElementsByTagName('tr');
+
+    for (let i = 0; i < tr.length; i++){
+    td = tr[i].getElementsByTagName('td')[0]
+    td2 = tr[i].getElementsByTagName('td')[1]
+    if (td || td2){
+        txtValue = td.textContent || td.innerText;
+        txtValue2 = td2.textContent || td2.innerText;
+
+        if(txtValue.toUpperCase().indexOf(filter) > -1 || txtValue2.toUpperCase().indexOf(filter) > -1) {
+            tr[i].style.display = ''
+
+            } else {
+                tr[i].style.display = 'none'
+            }
+        }
+    }
+}
